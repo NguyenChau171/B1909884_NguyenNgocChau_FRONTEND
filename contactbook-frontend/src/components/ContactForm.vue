93709665b0a474f78df9ebf2c7fd3c1c144b0f91
NguyenChau171 <ngocchaustst123@gmail.com>
2023-04-07 15:05:48 +0700

    Tao trang them moi lien he

--- contactbook-frontend/src/components/ContactForm.vue
+++ contactbook-frontend/src/components/ContactForm.vue
@@ -1,72 +1,76 @@
 <template>
-    <Form
-    @submit="submitContact"
-    :validation-schema="contactFormSchema"
-    >
-    <div class="form-group">
-    <label for="name">Tên</label>
-    <Field
-    name="name"
-    type="text"
-    class="form-control"
-    v-model="contactLocal.name"
-    />
-    <ErrorMessage name="name" class="error-feedback" />
-    </div>
-    <div class="form-group">
-    <label for="email">E-mail</label>
-    <Field
-    name="email"
-    type="email"
-    class="form-control"
-    v-model="contactLocal.email"
-    />
-    <ErrorMessage name="email" class="error-feedback" />
-    </div>
-<div class="form-group">
-<label for="address">Địa chỉ</label>
-<Field
-name="address"
-type="text"
-class="form-control"
-v-model="contactLocal.address"
-/>
-<ErrorMessage name="address" class="error-feedback" />
-</div>
-<div class="form-group">
-<label for="phone">Điện thoại</label>
-<Field
-name="phone"
-type="tel"
-class="form-control"
-v-model="contactLocal.phone"
-/>
-<ErrorMessage name="phone" class="error-feedback" />
-</div>
-<div class="form-group form-check">
-<input
-name="favorite"
-type="checkbox"
-class="form-check-input"
-v-model="contactLocal.favorite"
-/>
-<label for="favorite" class="form-check-label">
-<strong>Liên hệ yêu thích</strong>
-    </label>
-    </div>
-    <div class="form-group">
-    <button class="btn btn-primary">Lưu</button>
-    <button
-    v-if="contactLocal._id"
-    type="button"
-    class="ml-2 btn btn-danger"
-    @click="deleteContact"
-    >
-    Xóa
-    </button>
-    </div>
+    <Form @submit="submitContact" :validation-schema="contactFormSchema">
+        <div class="form-group">
+            <label for="name">Tên</label>
+            <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
+            <ErrorMessage name="name" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
+            <ErrorMessage name="email" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="address">Địa chỉ</label>
+            <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
+            <ErrorMessage name="address" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="phone">Điện thoại</label>
+            <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
+            <ErrorMessage name="phone" class="error-feedback" />
+        </div>
+        <div class="form-group form-check">
+            <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
+            <label for="favorite" class="form-check-label">
+                <strong>Liên hệ yêu thích</strong>
+            </label>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Lưu</button>
+            <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
+                Xóa
+            </button>
+        </div>
     </Form>
 </template>
+<template>
+    <Form @submit="submitContact" :validation-schema="contactFormSchema">
+        <div class="form-group">
+            <label for="name">Tên</label>
+            <Field name="name" type="text" class="form-control" v-model="contactLocal.name" />
+            <ErrorMessage name="name" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="email">E-mail</label>
+            <Field name="email" type="email" class="form-control" v-model="contactLocal.email" />
+            <ErrorMessage name="email" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="address">Địa chỉ</label>
+            <Field name="address" type="text" class="form-control" v-model="contactLocal.address" />
+            <ErrorMessage name="address" class="error-feedback" />
+        </div>
+        <div class="form-group">
+            <label for="phone">Điện thoại</label>
+            <Field name="phone" type="tel" class="form-control" v-model="contactLocal.phone" />
+            <ErrorMessage name="phone" class="error-feedback" />
+        </div>
+        <div class="form-group form-check">
+            <input name="favorite" type="checkbox" class="form-check-input" v-model="contactLocal.favorite" />
+            <label for="favorite" class="form-check-label">
+                <strong>Liên hệ yêu thích</strong>
+            </label>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">Lưu</button>
+            <button v-if="contactLocal._id" type="button" class="ml-2 btn btn-danger" @click="deleteContact">
+                Xóa
+            </button>
+        </div>
+    </Form>
+</template>
+
 <script>
 import * as yup from "yup";
 import { Form, Field, ErrorMessage } from "vee-validate";
@@ -86,7 +90,7 @@ export default {
                 .string()
                 .required("Tên phải có giá trị.")
                 .min(2, "Tên phải ít nhất 2 ký tự.")
-                .max(50, "Tên có nhiều nhất 50 ký tự."),
+                .max(50, "Tên có nhiều nhất là 50 ký tự."),
             email: yup
                 .string()
                 .email("E-mail không đúng.")
@@ -94,17 +98,15 @@ export default {
             address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
             phone: yup
                 .string()
-                .matches(
-                    /((09|03|07|08|05)+([0-9]{8})\b)/g,
-                    "Số điện thoại không hợp lệ."
-                ),
+                .matches(/((09|03|07|08|05)+([0-9]{8})\b)/g,
+                    "Số điện thoại không hợp lệ."),
         });
         return {
             // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
             // contactLocal để liên kết với các input trên form
             contactLocal: this.contact,
             contactFormSchema,
-            };
+        };
     },
     methods: {
         submitContact() {
@@ -116,6 +118,7 @@ export default {
     },
 };
 </script>
+
 <style scoped>
 @import "@/assets/form.css";
 </style>
